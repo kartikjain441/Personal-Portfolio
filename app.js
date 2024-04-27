6d7eb9a7bf1d81f6f54b5058d1958e39560a6b10
kartikjain441 <kartikjaink101@gmail.com>
2024-04-27 13:08:04 +0530

    Add file

---
+++ app.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const bar = document.getElementById("bar");
+const navbar = document.querySelector(".navbar");
+const close = document.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    console.log("clicked");
+    // Toggle the visibility of the navbar
+    navbar.style.visibility = "visible";
+    bar.style.visibility = "hidden";
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    navbar.style.display = "none";
+  });
+}
